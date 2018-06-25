400e8c03915e8634cccbaa3b4a1a182231e164f3
Caren Keyes <cejkeyes@gmail.com>
2018-06-25 11:46:46 -0700

    add tests for makeGuess

--- src/reducer.test.js
+++ src/reducer.test.js
@@ -39,5 +39,40 @@ describe('reducer', () => {
             expect(state.guesses).toEqual([50, 25, 75])
         })
 
+        it('Should give feedback if guess is correct', () => {
+            guess = state.correctAnswer;
+            feedback = 'You got it!'
+            state = reducer(state, makeGuess(guess));
+            expect(state.feedback).toEqual(feedback)
+        })
+
+        it('Should give hot feedback if closer than 10', () => {
+            guess = state.correctAnswer + 5;
+            feedback = "You're Hot!";
+            state = reducer(state, makeGuess(guess));
+            expect(state.feedback).toEqual(feedback);
+        })
+
+        it('Should give warm feedback if closer than 30', () => {
+            guess = state.correctAnswer + 25;
+            feedback = "You're Warm.";
+            state = reducer(state, makeGuess(guess));
+            expect(state.feedback).toEqual(feedback);
+        })
+
+        it('Should give cold feedback if between 30 and 50', () => {
+            guess = state.correctAnswer + 35;
+            feedback = "You're Cold...";
+            state = reducer(state, makeGuess(guess));
+            expect(state.feedback).toEqual(feedback);
+        })
+
+        it('Should give ice cold feedback if more than 50', () => {
+            guess = state.correctAnswer + 55;
+            feedback = "You're Ice Cold...";
+            state = reducer(state, makeGuess(guess));
+            expect(state.feedback).toEqual(feedback);
+        })
+
     })
 })
